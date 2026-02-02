a61815c69d32413dc56278eb17809396f083aabe
gaab0418 <zgabrielw.sz13@gmail.com>
2026-02-02 20:39:25 -0300

    Config de Cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
 	FastifyAdapter,
 	NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestFastifyApplication>(
@@ -11,8 +12,24 @@ async function bootstrap() {
 		new FastifyAdapter(),
 	);
 
+	const configService = app.get(ConfigService);
+	const nodeEnv = configService.get<string>('NODE_ENV', 'development');
+
+	if (nodeEnv !== 'production') {
+		app.enableCors();
+	} else {
+		const allowedOrigins =
+			configService.get<string>('ALLOWED_ORIGINS')?.split(',') || [];
+		app.enableCors({
+			origin: allowedOrigins,
+			methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,UPDATE',
+			credentials: true,
+		});
+	}
+
 	app.setGlobalPrefix('api/v1');
 
-	await app.listen(process.env.PORT ?? 3000);
+	const port = configService.get<number>('SERVER_PORT', 3000);
+	await app.listen(port, '0.0.0.0');
 }
 bootstrap();
